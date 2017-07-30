7c56e3a37d22c7c0be2e6419acfb912d19247222
Akl <sakl@deloitte.ie>
2017-07-30 10:19:28 +0100

    Login successful.

--- salah_rent/salah_rent.py
+++ salah_rent/salah_rent.py
@@ -20,3 +20,69 @@ def connect_db():
 	db.row_factory = sqlite3.Row
 	return db
 
+# Open a new database connection
+def get_db():
+	if not hasattr(g, 'sqlite_db'):
+		g.sqlite_db = connect_db()
+	return g.sqlite_db
+
+# Create database
+def init_db():
+	db = get_db()
+	with app.open_resource('schema.sql', mode='r') as f:
+		db.cursor().executescript( f.read() )
+	db.commit()
+
+# Add cmd command to initilaise the database
+@app.cli.command('initdb')
+def initdb_cmd():
+	init_db()
+	print('Initialised the database.')
+
+# Close database
+@app.teardown_appcontext
+def close_db(error):
+	if hasattr(g, 'sqlite_db'):
+		g.sqlite_db.close()
+
+#
+# Views
+#
+
+@app.route('/')
+def main_redirect():
+	if session.get('logged_in'):
+		return redirect( url_for('my_profile') )
+
+	return redirect( url_for('login') )
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		db = get_db()
+		user = request.form['username']
+		password = request.form['password']
+		userID = None
+
+		# Check password
+		cursor = db.execute('SELECT password, user_id FROM users WHERE username = ?', [request.form['username']])
+		user = cursor.fetchone()
+		if user:
+			userID = user['user_id']
+			if password != user['password']:
+				error = 'Your password is incorrect'
+			else:
+				session['logged_in'] = True
+				flash('Log in successful')
+				return redirect( url_for('my_profile') )
+		else:
+			error = 'Invalid Username'
+	return render_template('login.html', error=error)
+
+# @app.route('my_profile', methods=['GET', 'POST'])
+# def profile():
+# 	error = None
+# 	if not session.get('logged_in'):
+# 		error = 'Please log in to continue.'
+# 	
--- salah_rent/schema.sql
+++ salah_rent/schema.sql
@@ -2,17 +2,17 @@ DROP TABLE IF EXISTS `users_info`;
 DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
-	`user_id`	INT(5) autoincrement,
-	`username`	VARCHAR(50),
-	`password`	VARCHAR(255),
-
-	PRIMARY KEY `entries_pk` (`user_id`)
+	`user_id`	INTEGER PRIMARY KEY autoincrement,
+	`username`	VARCHAR,
+	`password`	VARCHAR
 );
 
 CREATE TABLE `users_info` (
-	`user_id`	INT(5),
-	`nickname`	VARCHAR(50),
+	`user_id`	INT PRIMARY KEY,
+	`nickname`	VARCHAR,
 
-	PRIMARY KEY `users_info_pk` (`user_id`),
-	FOREIGN KEY `users_info-users_fk` (user_id) REFERENCES `users`(user_id)
+	FOREIGN KEY(user_id) REFERENCES `users`(user_id)
 );
+
+INSERT INTO `users` (username, password)
+VALUES ('admin', '1234');
